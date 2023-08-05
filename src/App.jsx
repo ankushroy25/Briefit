ffbc89808ed53736a18278480bdb08cf2de03823
Ankush Roy <imankushroy@gmail.com>
2023-08-05 19:43:20 +0530

    updated logo, ui

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,7 @@ import Hero from "./components/Hero";
 import "./App.css";
 const App = () => {
   return (
-    <main>
-      <div className="main">
-        <div className="gradient" />
-      </div>
+    <main className="bg-mesh">
       <div className="app">
         <Hero />
         <Demo />
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,18 +4,19 @@ import { logo } from "../assets";
 const Hero = () => {
   return (
     <header className="w-full flex justify-center items-center flex-col">
-      <nav className="w-full flex justify-between items-center mb-10 pt-4">
-        <img src={logo} className="w-30 object-contain" />
+      <nav className="w-full flex justify-between items-center mb-4 pt-4">
+        <img src={logo} className="object-contain" />
+        <h1 className="head_text mr-8">BriefIt</h1>
         <button
           type="button"
-          onClick={() => window.open("https://github.com/ankushroy25")}
+          onClick={() => window.open("https://github.com/ankushroy25/briefit")}
         >
-          Github
+          <b className="text-2xl"> Code</b>
         </button>
       </nav>
-      <h1 className="head_text">
-        Brief articles,blogs <br className="max-md:hidden" />
-        <span className="orange_gradient">OpenAI GPT</span>
+      <h1 className="text-4xl md:text-5xl text-center font-bold blue_gradient pb-2">
+        Summarize news, articles, blogs <br className="max-md:hidden" />
+        <span>GPT powered</span>
       </h1>
       <h2 className="desc">
         Read your blogs and articles faster with our summarizer that makes it
